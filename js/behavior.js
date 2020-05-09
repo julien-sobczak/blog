aa55146429ab53ea77c5bd88129f272b5d8101ca
Julien Sobczak <julien.sobczak@gmail.com>
2020-05-09 11:58:35 +0200

    Remove blurring effect on post figures

--- js/behavior.js
+++ js/behavior.js
@@ -31,34 +31,6 @@ if (postList) {
     });
 
 
-    // Inspired by https://www.sitepoint.com/scroll-based-animations-jquery-css3/
-    // Un-blur figures when first appearing on screen
-    var $animation_elements = $('figure img');
-    $window.on('scroll resize', check_if_in_view);
-    $window.trigger('scroll');
-    function check_if_in_view() {
-      var window_height = $window.height();
-      var window_top_position = $window.scrollTop();
-      var window_bottom_position = (window_top_position + window_height);
-
-      $.each($animation_elements, function() {
-        var $element = $(this);
-        var element_height = $element.outerHeight();
-        var element_top_position = $element.offset().top;
-        var element_bottom_position = (element_top_position + element_height);
-
-        // check to see if this current container is within viewport
-        if ((element_bottom_position >= window_top_position) &&
-            (element_top_position <= window_bottom_position)) {
-          $element.addClass('in-view');
-        } //else {
-        //  $element.removeClass('in-view');
-        //}
-      });
-    }
-
-   
-
     // Activate the sunshine effect on the main label list
     $('#labels .label').hover(
       function() {
