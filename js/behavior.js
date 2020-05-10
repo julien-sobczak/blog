1cd709fb98594e0a6330ded8f6a82909e83e6227
Julien Sobczak <julien.sobczak@gmail.com>
2020-05-10 12:20:42 +0200

    Reimplement masonry effect using CSS columns

--- js/behavior.js
+++ js/behavior.js
@@ -1,13 +1,13 @@
 
-// Enable Masonry effect
-var postList = document.getElementById('post-list');
-if (postList) {
-  new AnimOnScroll(postList, {
-	  minDuration: 0.4,
-	  maxDuration: 0.7,
-	  viewportFactor: 0.4
-  });
-}
+// // Enable Masonry effect
+// var postList = document.getElementById('post-list');
+// if (postList) {
+//   new AnimOnScroll(postList, {
+// 	  minDuration: 0.4,
+// 	  maxDuration: 0.7,
+// 	  viewportFactor: 0.4
+//   });
+// }
 
 window.addEventListener('load', (event) => {
   
